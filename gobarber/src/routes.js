167bfe05f26c590cf1ef9b3f4b422d19877fab55
Mayk Brito <maykbrito@gmail.com>
2019-06-27 09:42:28 -0300

    part02

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,7 +1,7 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
-module.exports = routes;
+export default routes;
