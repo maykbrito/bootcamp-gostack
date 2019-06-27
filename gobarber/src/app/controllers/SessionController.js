b2f2c5f79080bdcf82c354b3b1640c11b1165c01
Mayk Brito <maykbrito@gmail.com>
2019-06-27 14:31:40 -0300

    validation

--- gobarber/src/app/controllers/SessionController.js
+++ gobarber/src/app/controllers/SessionController.js
@@ -6,6 +6,17 @@ import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation Fails!"})
+    }
+
     const {email, password} = req.body;
 
     const user = await User.findOne({where : {email}})
