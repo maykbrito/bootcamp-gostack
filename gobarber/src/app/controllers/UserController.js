e77fe4b88fe9dc5ccc4e6b3a7d9e2dd7d0745246
Mayk Brito <maykbrito@gmail.com>
2019-06-27 14:19:12 -0300

    user update

--- gobarber/src/app/controllers/UserController.js
+++ gobarber/src/app/controllers/UserController.js
@@ -10,7 +10,24 @@ class UserController {
   }
 
   async update (req, res) {
-    return res.json({})
+    const {email, oldPassword} = req.body
+
+    const user = await User.findByPk(req.userId)
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email }})
+      if (userExists) return res.status(400).json({ error: "User Already Exists"})
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: "Password does not match"})
+    }
+
+
+    const {id, name, provider} = await user.update(req.body);
+
+    return res.json({id, name, email, provider});
+
   }
 }
 
