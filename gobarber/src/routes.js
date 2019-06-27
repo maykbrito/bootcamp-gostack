aa54b54d60ff139d5563e4ee690c2d383174be7c
Mayk Brito <maykbrito@gmail.com>
2019-06-27 13:11:20 -0300

    loaders model

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Mayk Brito',
+    email: 'mayk@brito.net.br',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
