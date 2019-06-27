8e86c3af170c72a663118c996116162f73d105f2
Mayk Brito <maykbrito@gmail.com>
2019-06-27 13:33:52 -0300

    hashing passwords

---
+++ gobarber/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es6:true,
+    node: true,
+  },
+  extends: 'webos',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion:2018,
+    sourceType: 'module'
+  },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
+  }
+}
--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController'
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Mayk Brito',
-    email: 'mayk@brito.net.br',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store)
 
 export default routes;
