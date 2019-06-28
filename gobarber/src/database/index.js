05606c9377fe3462dbba943be13968a4bb6cfe97
Mayk Brito <maykbrito@gmail.com>
2019-06-28 18:14:54 -0300

    chore: add user avatar

--- gobarber/src/database/index.js
+++ gobarber/src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -13,7 +14,8 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models.map(model => model.init(this.connection))
+    .map(model => model.associate && model.associate(this.connection.models))
   }
 }
 
