e695dfb6396d3e31ff8e542498774e8f191713ee
Mayk Brito <maykbrito@gmail.com>
2019-06-28 18:29:16 -0300

    feature: providers list

--- gobarber/src/app.js
+++ gobarber/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 import './database';
 
@@ -13,6 +14,9 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use('/files', express.static(
+      path.resolve(__dirname, '..', 'tmp', 'uploads')
+    ))
   }
 
   routes() {
