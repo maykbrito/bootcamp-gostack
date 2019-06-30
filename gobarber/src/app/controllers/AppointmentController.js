ae9e0bbd3880666b773e15224a7115da2dd70e1d
Mayk Brito <maykbrito@gmail.com>
2019-06-30 12:43:50 -0300

    feature: add pagination

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -7,6 +7,9 @@ import File from '../models/File'
 
 class AppointmentController {
   async index( req, res ) {
+
+    const { page = 1 } = req.query
+
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
@@ -14,6 +17,8 @@ class AppointmentController {
       },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page -1) * 20,
       include: [
         {
           model: User,
