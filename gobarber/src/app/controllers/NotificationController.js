2450836317dd927c417af9e667a7314e4a5a6c30
Mayk Brito <maykbrito@gmail.com>
2019-06-30 14:54:27 -0300

    feature: mark a notification as read

--- gobarber/src/app/controllers/NotificationController.js
+++ gobarber/src/app/controllers/NotificationController.js
@@ -17,6 +17,16 @@ class NotificationController {
 
     return res.json(notifications)
   }
+
+  async update (req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true}
+    )
+
+    return res.json(notification)
+  }
 }
 
 export default new NotificationController();
