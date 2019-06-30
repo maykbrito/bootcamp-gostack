5ddfefbd30116ddd5d0da1dfbe023a28905f1dae
Mayk Brito <maykbrito@gmail.com>
2019-06-30 14:26:54 -0300

    feature: add notification

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,9 +1,10 @@
 import * as Yup from 'yup'
-import { startOfHour, parseISO, isBefore} from 'date-fns'
-
+import { startOfHour, parseISO, isBefore, format} from 'date-fns'
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment'
 import User from '../models/User'
 import File from '../models/File'
+import Notification from '../schemas/Notification'
 
 class AppointmentController {
   async index( req, res ) {
@@ -84,6 +85,19 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date
+    })
+
+    // Notify provider
+    const user = await User.findByPk(req.userId);
+    const formmatedDate = format(
+      hourStart,
+      "dd 'de' MMMM', às' H:mm'h'",
+      {locale: pt}
+    )
+
+    Notification.create({
+      content: `Novo agendamento de ${user.name} para dia ${formmatedDate}`,
+      user: provider_id
     })
 
     return res.json(appointment);
