8c7431c245022eff698189e311d2a9404dd5fa9d
Mayk Brito <maykbrito@gmail.com>
2019-06-30 13:30:41 -0300

    feature: add mongodb

--- gobarber/src/database/index.js
+++ gobarber/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,12 +12,20 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
     models.map(model => model.init(this.connection))
     .map(model => model.associate && model.associate(this.connection.models))
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true}
+    )
   }
 }
 
