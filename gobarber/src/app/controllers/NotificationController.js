ce7c7905ca334ce8b349448209443bb6d1f16709
Mayk Brito <maykbrito@gmail.com>
2019-06-30 14:47:11 -0300

    feature: list user notifications

---
+++ gobarber/src/app/controllers/NotificationController.js
@@ -0,0 +1,22 @@
+import Notification from '../schemas/Notification'
+import User from '../models/User'
+
+class NotificationController {
+  async index(req, res) {
+    const isProvider = await User.findOne({ where: {
+      id: req.userId,
+      provider: true
+    }})
+
+    if (!isProvider)
+      return res.status(401).json({ error: 'Only provider can load notifications'})
+
+    const notifications = await Notification.find({ user: req.userId})
+    .sort({createdAt: 'desc'})
+    .limit(20)
+
+    return res.json(notifications)
+  }
+}
+
+export default new NotificationController();
