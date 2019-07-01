c07989081ae6d899099e1c7ab94b82f4c9d20729
Mayk Brito <maykbrito@gmail.com>
2019-07-01 08:18:29 -0300

    feature: send Mail using Queue Jobs with redis

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -5,7 +5,8 @@ import Appointment from '../models/Appointment'
 import User from '../models/User'
 import File from '../models/File'
 import Notification from '../schemas/Notification'
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail'
 
 class AppointmentController {
   async index( req, res ) {
@@ -141,20 +142,7 @@ class AppointmentController {
 
     await appointment.save()
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(
-          appointment.date,
-          "dd 'de' MMMM', às' H:mm'h'",
-          {locale: pt}
-        )
-      }
-    })
+    await Queue.add(CancellationMail.key, { appointment })
 
     return res.json(appointment)
   }
