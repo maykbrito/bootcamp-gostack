4d737ccbfafd77f835dbd959b5f80536348d6fc1
Mayk Brito <maykbrito@gmail.com>
2019-07-01 19:06:41 -0300

    feature: mail lib

---
+++ meetapp/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  secure: false,
+  default: {
+    from: 'Equipe MeetApp <mail@meet.app>',
+  },
+};
