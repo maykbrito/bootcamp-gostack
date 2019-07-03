9be2a99aa0dac269463f0551eedb2f7214fc90e5
Mayk Brito <maykbrito@gmail.com>
2019-07-03 08:38:47 -0300

    chore: added App.js and components

---
+++ desafio-04/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import Posts from './components/Posts';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Posts />
+    </>
+  );
+}
+
+export default App;
