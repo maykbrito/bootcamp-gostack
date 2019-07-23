fbd846efa352f3961fc47dd47ebedd4718c296d7
maykbrito <maykbrito@gmail.com>
2019-07-23 09:44:48 -0400

    chore: add eslint prettier editor

--- gobarber/.eslintrc.js
+++ gobarber/.eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
   env: {
-    es6:true,
+    es6: true,
     node: true,
   },
-  extends: 'wesbos',
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion:2018,
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   rules: {
-    "no-shadow": "off",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
-  }
-}
+    'prettier/prettier': 'error',
+    'no-shadow': 'off',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-console': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
