c841164d3696f34a6548028b9a4eb697ecad38a5
maykbrito <maykbrito@gmail.com>
2019-07-23 09:59:17 -0400

    chore: add avatar to session

--- gobarber/src/app/controllers/SessionController.js
+++ gobarber/src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -19,7 +20,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -29,13 +39,14 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
