ff38d4106e4a824b37775d99144984c5288f0723
maykbrito <maykbrito@gmail.com>
2019-07-23 09:21:42 -0400

    feature: add cors

--- gobarber/src/app.js
+++ gobarber/src/app.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import * as Sentry from '@sentry/node';
-import path from 'path';
-import Youch from 'youch';
-import sentryConfig from './config/sentry';
-import 'express-async-errors';
-import routes from './routes';
+import express from "express";
+import * as Sentry from "@sentry/node";
+import path from "path";
+import cors from "cors";
+import Youch from "youch";
+import sentryConfig from "./config/sentry";
+import "express-async-errors";
+import routes from "./routes";
 
-import './database';
+import "./database";
 
 class App {
   constructor() {
@@ -22,9 +23,10 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(
-      '/files',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+      "/files",
+      express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
     );
   }
 
@@ -35,12 +37,12 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      if (process.env.NODE_ENV === 'development') {
+      if (process.env.NODE_ENV === "development") {
         const errors = await new Youch(err, req).toJSON();
         return res.status(500).json(errors);
       }
 
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: "Internal Server Error" });
     });
   }
 }
